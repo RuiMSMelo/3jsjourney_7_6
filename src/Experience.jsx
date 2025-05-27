a3977c1e5e574c09bacd2a8ebc01b1963ae39ac0
RuiMSMelo <ruimsmelo@gmail.com>
2025-05-27 11:34:07 +0100

    environment maps

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,6 +1,7 @@
 import { useThree, useFrame } from '@react-three/fiber'
 import { useEffect, useRef } from 'react'
 import {
+    Lightformer,
     Environment,
     Sky,
     ContactShadows,
@@ -30,8 +31,8 @@ export default function Experience() {
     const { shadowsColor, shadowsOpacity, shadowsBlur } = useControls(
         'Contact Shadow',
         {
-            shadowsColor: '#1039a0',
-            shadowsOpacity: { value: 0.4, min: 0, max: 1, step: 0.01 },
+            shadowsColor: '#4b2709',
+            shadowsOpacity: { value: 0.6, min: 0, max: 1, step: 0.01 },
             shadowsBlur: { value: 2.8, min: 0, max: 10, step: 0.01 },
         }
     )
@@ -39,9 +40,13 @@ export default function Experience() {
     //     sunPosition: { value: [1, 2, 3] },
     // })
 
-    const { envMapIntensity } = useControls('environment map', {
-        envMapIntensity: { value: 1, min: 0, max: 12 },
-    })
+    const { envMapIntensity, envMapHeight, envMapRadius, envMapScale } =
+        useControls('environment map', {
+            envMapIntensity: { value: 1, min: 0, max: 12 },
+            envMapHeight: { value: 7, min: 0, max: 100 },
+            envMapRadius: { value: 28, min: 10, max: 1000 },
+            envMapScale: { value: 100, min: 10, max: 1000 },
+        })
 
     const scene = useThree((state) => state.scene)
     useEffect(() => {
@@ -51,17 +56,35 @@ export default function Experience() {
     return (
         <>
             <Environment
-                background
-                files={[
-                    './environmentMaps/2/px.jpg',
-                    './environmentMaps/2/nx.jpg',
-                    './environmentMaps/2/py.jpg',
-                    './environmentMaps/2/ny.jpg',
-                    './environmentMaps/2/pz.jpg',
-                    './environmentMaps/2/nz.jpg',
-                ]}
-                envMapIntensity
-            />
+                // files={[
+                //     './environmentMaps/2/px.jpg',
+                //     './environmentMaps/2/nx.jpg',
+                //     './environmentMaps/2/py.jpg',
+                //     './environmentMaps/2/ny.jpg',
+                //     './environmentMaps/2/pz.jpg',
+                //     './environmentMaps/2/nz.jpg',
+                // ]}
+                // files={'./environmentMaps/the_sky_is_on_fire_2k.hdr'}
+                preset='sunset'
+                intensity={envMapIntensity}
+                ground={{
+                    height: envMapHeight,
+                    radius: envMapRadius,
+                    scale: envMapScale,
+                }}
+            >
+                {/* <Lightformer
+                    position-z={-5}
+                    scale={5}
+                    color='red'
+                    intensity={10}
+                    form='ring'
+                /> */}
+                {/* <mesh position-z={-5} scale={10}>
+                    <planeGeometry />
+                    <meshBasicMaterial color={[10, 0, 0]} />
+                </mesh> */}
+            </Environment>
 
             {/* <BakeShadows /> */}
             {/* <SoftShadows size={25} samples={10} focus={0} /> */}
@@ -85,7 +108,7 @@ export default function Experience() {
             </AccumulativeShadows> */}
 
             <ContactShadows
-                position={[0, -0.99, 0]}
+                position={[0, 0, 0]}
                 scale={10}
                 resolution={512}
                 far={5}
@@ -116,20 +139,26 @@ export default function Experience() {
 
             {/* <Sky sunPosition={sunPosition} /> */}
 
-            <mesh castShadow position-x={-2}>
+            <mesh castShadow position-x={-2} position-y={1}>
                 <sphereGeometry />
                 <meshStandardMaterial color='orange' />
             </mesh>
 
-            <mesh castShadow ref={cubeRef} position-x={2} scale={1.5}>
+            <mesh
+                castShadow
+                ref={cubeRef}
+                position-x={2}
+                scale={1.5}
+                position-y={1}
+            >
                 <boxGeometry />
                 <meshStandardMaterial color='mediumpurple' />
             </mesh>
 
-            <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
+            {/* <mesh position-y={0} rotation-x={-Math.PI * 0.5} scale={10}>
                 <planeGeometry />
                 <meshStandardMaterial color='greenyellow' />
-            </mesh>
+            </mesh> */}
         </>
     )
 }
